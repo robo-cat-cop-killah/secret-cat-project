60842cd7572b6cca54d3f47a9105212fa299582d
robo-cat-cop-killah <84450657+robo-cat-cop-killah@users.noreply.github.com>
2021-05-21 02:56:29 +0200

    Init

---
+++ test-cases/__tests__/test-start-page.js
@@ -0,0 +1,48 @@
+const puppeteer = require('puppeteer');
+jest.setTimeout(process.env.ASMA_GLOBAL_TIMEOUT);
+
+describe('(Start Page)',() =>
+{
+	
+	let pages;
+	let page;
+	let browser;
+	
+	beforeAll(async() => {
+		var retries = 10;
+		var count = 0;
+		do {
+			try {
+				browser = await puppeteer.connect({browserURL: "http://localhost:9222/json",timeout:1000});
+	   			pages = await browser.pages();
+	  			page = pages[0];
+		  	} catch (Exception) {
+
+		  	}
+	  	} while (browser == undefined && count < retries);
+	});
+
+	test("Open Apicasystems.com",async() => {	
+		try {
+			await page.goto("https://www.apica.io");
+
+		} catch (e) {
+  			if (e instanceof puppeteer.errors.TimeoutError) {
+    				throw new Error("Failed to load https://www.apica.io.");
+			}
+		}	
+	});
+
+	test("Click link",async() => {	
+		try {
+			await page.goto("https://www.digitaljournal.com");
+
+		} catch (e) {
+  			if (e instanceof puppeteer.errors.TimeoutError) {
+    				throw new Error("Failed to load https://www.digitaljournal.com");
+			}
+		}	
+	});	 
+
+});
+
