60822187686d00c9a4665f7c00cba86abbf8a9ec
robo-cat-cop-killah <84450657+robo-cat-cop-killah@users.noreply.github.com>
2021-05-24 05:30:04 +0200

    Start framebuffer

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
 const {exec} = require('child_process');
-
+exec("Xvfb :0");
 console.log("Starting asmator web browser ... ");
 exec(process.env.ASMA_CHROME_BIN + ' ' + process.env.ASMA_CHROME_ARGS);
 
