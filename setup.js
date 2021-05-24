5923d82e84539e549a46f8fb19ecf5713bfa29b6
robo-cat-cop-killah <84450657+robo-cat-cop-killah@users.noreply.github.com>
2021-05-24 20:25:03 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -4,6 +4,8 @@ const subprocess = spawn("Xvfb", [':0','-shmem'], {
   detached: true,
   stdio: 'ignore'
 });
+subprocess.unref();
+
 console.log("Starting asmator web browser ... ");
 exec(process.env.ASMA_CHROME_BIN + ' ' + process.env.ASMA_CHROME_ARGS);
 
